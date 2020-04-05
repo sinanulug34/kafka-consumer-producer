fc169d76404c9145644c871249b0a474737a91ec
Sinan Uluğ <sinanulug@aaas-MacBook-Pro.local>
2020-04-05 22:42:40 +0300

    fixed .pom xml error

--- src/main/java/com/kafka/procedur/consumer/KafkaTestApplication.java
+++ src/main/java/com/kafka/procedur/consumer/KafkaTestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkatestApplication {
+public class KafkaTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KafkatestApplication.class, args);
+		SpringApplication.run(KafkaTestApplication.class, args);
 	}
 
 }
--- src/test/java/com/kafka/procedur/consumer/KafkaIntegrationTests.java
+++
@@ -1,9 +0,0 @@
-package com.kafka.procedur.consumer;
-
-import com.kafka.procedur.consumer.configuration.KafkaConsumerConfiguration;
-
-import java.util.Properties;
-
-public class KafkaIntegrationTests {
-        //to do
-}
